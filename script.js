b46866aeec72866869ede6de3682441d7831f423
Your Name <you@example.com>
2021-01-06 15:50:01 -0800

    Added example for how to get 10 random people from randomuser.me/api

--- script.js
+++ script.js
@@ -1,28 +1,28 @@
 // fetch().then().then().catch()
 // .then is attached to something, and whatever the previous thing is returning is what its callback function's parameter is.
 
-const redditEndpoint = 'https://www.reddit.com/search.json?q=kittens'
+
+//// Code for requesting information from Reddit. ////
+// const redditEndpoint = 'https://www.reddit.com/search.json?q=kittens'
+// console.log("Hello from Fetch Practice!")
+
+// fetch(redditEndpoint) // Can also put in the URL link directly.
+// .then((fetchObj) => {
+//     console.log(fetchObj)
+//     return fetchObj.json()
+// })
+// .then((jsonData) => {
+//     console.log('Here is the json data:', jsonData) // This gets printed after 'Just fired the AJAX request because the request takes a while.
+// })
+// .catch((error) => {
+// console.log("Oh no, you did NOT make fetch happen")
+// console.log(error)
+// })
+
+// console.log("Just fired the AJAX request")
 
 
-
-console.log("Hello from Fetch Practice!")
-
-fetch(redditEndpoint)
-.then((fetchObj) => {
-    console.log(fetchObj)
-    return fetchObj.json()
-})
-.then((jsonData) => {
-    console.log('Here is the json data:', jsonData) // This gets printed after 'Just fired the AJAX request because the request takes a while.
-})
-.catch((error) => {
-console.log("Oh no, you did NOT make fetch happen")
-console.log(error)
-})
-
-console.log("Just fired the AJAX request")
-
-
+//// Sample code to understand callback functions. ////
 // const taylorsFunction = (bands) => {
 //     bands.foEach((band) => {
 //         console.log(band)
@@ -30,3 +30,18 @@ console.log("Just fired the AJAX request")
 // }
 
 // taylorsFunction(['Hot Mulligan', 'Local Natives'])
+
+
+
+const randomUserEndpoint = 'https://randomuser.me/api/?results=10' // Add ?results=10 to URL get 10 results. 
+
+fetch(randomUserEndpoint)
+.then((fetchedUsers) => {
+    return fetchedUsers.json()
+})
+.then((jsonUsers) => {
+    console.log(jsonUsers)
+})
+.catch((error) => {
+    console.log('Failed to fetch users', error)
+})
